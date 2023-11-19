9233beb416abbb36af79124cbabc8ab0b9cca6a9
Rashid KC <rashid.kc@hexeam.in>
2023-11-19 16:05:15 +0530

    Added Core Data Helpers for Issue and Tag. Called added variables in Sidebar View

---
+++ FeedbackAssistant/Issue-CoreDataHelpers.swift
@@ -0,0 +1,58 @@
+//
+//  Issue-CoreDataHelpers.swift
+//  FeedbackAssistant
+//
+//  Created by Rashid KC on 19/11/23.
+//
+
+import Foundation
+
+extension Issue {
+    var issueTitle: String {
+        get { title ?? "" }
+        set { title = newValue }
+    }
+    
+    var issueContent: String {
+        get { content ?? "" }
+        set { content = newValue }
+    }
+    
+    var issueCreationDate: Date {
+        creationDate ?? .now
+    }
+    
+    var issueModificationDate: Date {
+        creationDate ?? .now
+    }
+    
+    var issueTags: [Tag] {
+        let result = tags?.allObjects as? [Tag] ?? []
+        return result.sorted()
+    }
+    
+    static var example: Issue {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let issue = Issue(context: viewContext)
+        issue.title = "Example issue"
+        issue.content = "This is an example issue"
+        issue.priority = 1
+        issue.creationDate = .now
+        return issue
+    }
+}
+
+extension Issue: Comparable {
+    public static func < (lhs: Issue, rhs: Issue) -> Bool {
+        let left = lhs.issueTitle.localizedLowercase
+        let right = rhs.issueTitle.localizedLowercase
+        
+        if left == right {
+            return lhs.issueCreationDate < rhs.issueCreationDate
+        } else {
+            return left < right
+        }
+    }
+}
