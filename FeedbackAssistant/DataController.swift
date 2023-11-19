e8af297b85774270e2368b1d48825c45f458a4c4
Rashid KC <rashid.kc@hexeam.in>
2023-11-19 22:38:46 +0530

    Editing issues

--- FeedbackAssistant/DataController.swift
+++ FeedbackAssistant/DataController.swift
@@ -11,6 +11,7 @@ class DataController: ObservableObject {
     let container: NSPersistentCloudKitContainer
     
     @Published var selectedFilter: Filter? = .all
+    @Published var selectedIssue: Issue?
     
     static var preview: DataController = {
         let dataController = DataController(inMemory: true)
@@ -93,5 +94,15 @@ class DataController: ObservableObject {
         delete(request2)
         
         save()
+    }
+    
+    func missingTags(from issue: Issue) -> [Tag] {
+        let request = Tag.fetchRequest()
+        let allTags = (try? container.viewContext.fetch(request)) ?? []
+
+        let allTagsSet = Set(allTags)
+        let difference = allTagsSet.symmetricDifference(issue.issueTags)
+
+        return difference.sorted()
     }
 }
---
+++ FeedbackAssistant/NoIssueView.swift
@@ -0,0 +1,26 @@
+//
+//  NoIssueView.swift
+//  FeedbackAssistant
+//
+//  Created by Rashid KC on 19/11/23.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        Text("No issue selected")
+            .font(.title)
+            .foregroundStyle(.secondary)
+        
+        Button("New issue") {
+            
+        }
+    }
+}
+
+#Preview {
+    NoIssueView()
+}
